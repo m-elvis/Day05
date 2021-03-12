a7f92a8c09fa477561934fd440f2cfd62fffa6ed
m-elvis <elvis.mahovic@gmail.com>
2021-03-12 22:37:43 +0100

    Doe

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    var lastName = "Doe";
+    var lastNameLength;
+    
+    lastNameLength = lastName.length;
+
+
+    return lastNameLength;
+}
+
+console.log(main());
+module.exports = main;

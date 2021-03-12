9e66ab21e59948ccc616cb80b9bf05061415281c
m-elvis <elvis.mahovic@gmail.com>
2021-03-12 23:54:38 +0100

    mynoun

---
+++ ex14/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+
+    var wordBlanks;
+
+    wordBlanks = myNoun + " " + myAdjective + " " + myVerb + " " + myAdverb + ".";
+
+
+    return wordBlanks;
+}
+    console.log(main());
+    module.exports = main;
